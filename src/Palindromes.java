7d77506d4dd84009a60f559d900864d6e61b3f4d
Арти <Ramilkontes@mail.ru>
2021-07-19 22:44:03 +0400

    Get polindromes

---
+++ src/Palindromes.java
@@ -0,0 +1,25 @@
+public class Palindromes {
+    private String word;
+    private boolean flag = false;
+    private char[] letters;
+
+    public Palindromes(String word) {
+        this.word = word;
+        this.letters = word.toCharArray();
+    }
+
+    public String definitionPolindromes()  {
+        int i1 = 0;
+        int i2 = letters.length - 1;
+        while (i2 > i1) {
+            if (letters[i1] != letters[i2]) {
+                flag = false;
+                return "This is false";
+            }
+            ++i1;
+            --i2;
+            flag = true;
+        }
+        return "This is true";
+    }
+}
